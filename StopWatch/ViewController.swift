39a172e00c8f81e2ca2c66bae8dc28f9e4120197
Haruka <daisy_smau@icloud.com>
2018-02-08 19:22:06 +0900

    selectorエラーでてますううう

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var label: UILabel!
+    
+    var counter: Float = 0.0
 
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func start() {
+        if !timer.isValid {
+            timer = Timer.scheduledTimer(timeInterval: 0.01,
+                                        target: self,
+                                        selector: #selector(self.up),
+                                        userInfo: nil,
+                                        repeats: true)
+        }
+        
+    }
+    
+    @IBAction func stop() {
+        if timer.isValid {
+            timer.invalidate()
+        }
+    }
+    
+    func up() {
+        counter += 0.01
+        label.text = String(format: "%.2f", counter)
+    }
 }
 
